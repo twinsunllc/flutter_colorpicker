7daafa5f35eb0e9968aaeea8e3c9918bbf5022e2
Jami Couch <jami.couch@twinsunsolutions.com>
2018-12-07 12:54:58 -0600

    Get rid of '0x' prefix for hex input, make color selection configurable, adjust sizing so that it looks good on big and small screens

--- lib/grid_picker.dart
+++ lib/grid_picker.dart
@@ -3,8 +3,9 @@
 library grid_colorpicker;
 
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-const List<Color> _colorTypes = [
+const List<Color> defaultColors = [
   Colors.red,
   Colors.pink,
   Colors.purple,
@@ -26,7 +27,6 @@ const List<Color> _colorTypes = [
   Colors.blueGrey,
   Colors.black,
 ];
-
 class GridPicker extends StatefulWidget {
   final List<Color> availableColors;
   final Color pickerColor;
@@ -37,8 +37,8 @@ class GridPicker extends StatefulWidget {
     @required this.pickerColor,
     @required this.onColorChanged,
     this.enableLabel: false,
-    this.availableColors = _colorTypes,
     this.showHexInput = false,
+    this.availableColors = defaultColors,
   });
 
   @override
@@ -50,32 +50,35 @@ class _GridPickerState extends State<GridPicker> {
   TextEditingController _hexController = new TextEditingController();
 
   double _viewWidth, _viewHeight;
+  final _crossAxisCount = 5;
 
   @override
   initState() {
     super.initState();
-    _currentColor = widget.availableColors[0];
-    _viewWidth = 4 * 70.0;
-    _viewHeight = widget.availableColors.length / 4 * 70.0;
+    _currentColor = widget.pickerColor ?? widget.availableColors[0];
+    _viewWidth = _crossAxisCount * 70.0;
+    _hexController.text = _currentColor.value.toRadixString(16).toUpperCase().replaceFirst('FF', '');
+    _viewHeight = widget.availableColors.length / _crossAxisCount * 70.0;
   }
 
   Widget _hexEntry() {
     return Container(
       padding: EdgeInsets.all(8.0),
       child: new ListTile(
-          title: new TextField(
-            textAlign: TextAlign.center,
-            controller: _hexController,
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.check),
-            onPressed: () {
-              var _hexValue = int.parse(_hexController.text);
-              _currentColor = new Color(_hexValue).withOpacity(1.0);
-              widget.onColorChanged(_currentColor);
-              Navigator.pop(context);
-            },
-          )),
+        title: new TextField(
+          textAlign: TextAlign.center,
+          controller: _hexController,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.check),
+          onPressed: () {
+            var _hexValue = int.parse('0x' + _hexController.text);
+            _currentColor = new Color(_hexValue).withOpacity(1.0);
+            widget.onColorChanged(_currentColor);
+            Navigator.pop(context);
+          },
+        ),
+      ),
     );
   }
 
@@ -84,57 +87,58 @@ class _GridPickerState extends State<GridPicker> {
     Orientation _orientation = MediaQuery.of(context).orientation;
 
     Widget _colorsGrid() {
+      var keySize = _orientation == Orientation.portrait ? MediaQuery.of(context).size.width - 80.0 : MediaQuery.of(context).size.height - 140.0;
+      var actualWidth = math.min(_viewWidth, keySize);
+      var actualHeight = math.min(_viewHeight, keySize);
       return Container(
-        width: _viewWidth,
-        height: _viewHeight,
+        width: actualWidth,
+        height: actualHeight,
         child: GridView.count(
-            crossAxisCount: 4,
-            scrollDirection: (_orientation == Orientation.portrait)
-                ? Axis.vertical
-                : Axis.horizontal,
-            children: []..addAll(widget.availableColors.map((Color _color) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _currentColor = _color;
-                      widget.onColorChanged(_currentColor);
-                      _hexController.text = _currentColor.value
-                          .toRadixString(16)
-                          .toUpperCase()
-                          .replaceFirst('FF', '0x');
-                    });
-                  },
-                  child: Container(
-                    color: Color(0),
-                    child: Align(
-                      child: Container(
-                        width: 45.0,
-                        height: 45.0,
-                        decoration: BoxDecoration(
-                          color: _color,
-                          borderRadius: BorderRadius.circular(5.0),
-                          boxShadow: (_currentColor == _color)
-                              ? [
-                                  (_color == Theme.of(context).cardColor)
-                                      ? BoxShadow(
-                                          color: Colors.grey[300],
-                                          blurRadius: 5.0,
-                                        )
-                                      : BoxShadow(
-                                          color: _color,
-                                          blurRadius: 5.0,
-                                        ),
-                                ]
-                              : null,
-                          border: (_color == Theme.of(context).cardColor)
-                              ? Border.all(color: Colors.grey[300], width: 1.0)
-                              : null,
+          crossAxisCount: _crossAxisCount,
+          scrollDirection: (_orientation == Orientation.portrait) ? Axis.vertical : Axis.horizontal,
+          children: []..addAll(
+              widget.availableColors.map(
+                (Color _color) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _currentColor = _color;
+                        widget.onColorChanged(_currentColor);
+                        _hexController.text = _currentColor.value.toRadixString(16).toUpperCase().replaceFirst('FF', '');
+                      });
+                    },
+                    child: Container(
+                      color: Color(0),
+                      child: Align(
+                        child: Container(
+                          width: 45.0,
+                          height: 45.0,
+                          decoration: BoxDecoration(
+                            color: _color,
+                            borderRadius: BorderRadius.circular(5.0),
+                            boxShadow: (_currentColor.value == _color.value)
+                                ? [
+                                    (_color == Theme.of(context).cardColor)
+                                        ? BoxShadow(
+                                            color: Colors.grey[300],
+                                            blurRadius: 5.0,
+                                          )
+                                        : BoxShadow(
+                                            color: _color,
+                                            blurRadius: 5.0,
+                                          ),
+                                  ]
+                                : null,
+                            border: (_color == Theme.of(context).cardColor) ? Border.all(color: Colors.grey[300], width: 1.0) : null,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              }))),
+                  );
+                },
+              ),
+            ),
+        ),
       );
     }
 
